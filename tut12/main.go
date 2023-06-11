6ead3fb114dfd4116826f711d240f5e143619370
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-06-11 22:51:29 +0100

    slice relationship with an array

--- tut12/main.go
+++ tut12/main.go
@@ -28,4 +28,19 @@ func main() {
 	for _, x := range sl1 {
 		pl(x)
 	}
+
+	// creating slices from an array
+	sArr := [5]int{1, 2, 3, 4, 5}
+	sl3 := sArr[0:2]
+	pl("first 3:", sArr[0:3])
+	pl("last 3:", sArr[2:])
+	pl("sl3 before: ", sl3)
+	sArr[0] = 10
+	pl("sl3 after :", sl3)
+
+	// appending to a slice created from an array
+	sl3 = append(sl3, 12)
+	pl("sl3 after appending: ", sl3)
+	pl("sArr after appending to sl3: ", sArr)
+
 }
